4974e8576b5ffe21e180d9852bee0bdb1dcedef4
Yehuda <playzyehuda@gmail.com>
2026-06-20 12:44:20 +0700

    Update v1.2

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA-R9mUjqlRYjmdNhXNcruSm5pwp4zzuIc",
+  authDomain: "laundry-tracker-1702.firebaseapp.com",
+  projectId: "laundry-tracker-1702",
+  storageBucket: "laundry-tracker-1702.firebasestorage.app",
+  messagingSenderId: "471546150515",
+  appId: "1:471546150515:web:a5298a92e1ec8269e7f406",
+};
